638ae55e77a9f2d29387f76caca1f5e49737772f
Ahmed Eldawy <ahmed.eldawy@badrit.com>
2010-07-06 18:07:16 +0300

    Finalized basic features

--- app/controllers/simple_desk_controller.rb
+++ app/controllers/simple_desk_controller.rb
@@ -2,30 +2,17 @@ class SimpleDeskController < ApplicationController
   before_filter :odesk_required
   
   def index
-    @work_diary = {}
-    RubyDesk::TeamRoom.get_teamrooms(@odesk_connector).each do |team_room|
-      work_diary = RubyDesk::Snapshot.work_diary(@odesk_connector, team_room.id, @odesk_connector.auth_user.uid)
-      @work_diary[team_room] = work_diary.first if work_diary
+    @time_report = RubyDesk::TimeReport.find(@odesk_connector,
+        :select=>"team_name, sum(hours)",
+        :conditions=>{:worked_on=>Date.today.monday, :provider_id=>@odesk_connector.auth_user.uid})
+    if @time_report && @time_report.size > 1
+      total_hours = @time_report.sum{|entry| entry['hours']}
+      @time_report.insert(0, {'team_name'=>"total", 'hours'=>total_hours})
     end
-
-    # Find profile of the current user
-    # There's no direct way to find current user profile
-    # Search for providers with the name and select one of them
-    # some_users = RubyDesk::Provider.search(@odesk_connector, "#{@odesk_connector.auth_user.first_name} #{@odesk_connector.auth_user.last_name}")
-    # if some_users
-    #   user_profile = some_users.find do |u|
-    #     u.dev_userid == @odesk_connector.auth_user.uid
-    #   end
-    # end
-    # 
-    # if user_profile
-    #   # Find one job for each skill for the profile (maximum 5 skills)
-    #   @jobs = {}
-    #   user_profile.skills[0,5].each do |skill|
-    #     jobs = RubyDesk::Job.search(@odesk_connector, skill.skl_name)
-    #     @jobs[skill] = jobs.first if jobs
-    #   end
-    # end
+  end
+  
+  def search_jobs
+    @jobs = RubyDesk::Job.search(@odesk_connector, params[:query])
   end
 
   def team_rooms
--- app/helpers/simple_desk_helper.rb
+++ app/helpers/simple_desk_helper.rb
@@ -1,2 +1,7 @@
 module SimpleDeskHelper
+  def format_hours(hours)
+    h = Integer(hours)
+    m = Integer(hours * 60) % 60
+    "%d:%02d" % [h, m]
+  end
 end
